d8003724c9bb7e3159000cc871bcbaf4ac3e2083
Cecilia Gripenberg <22261830+cicigee@users.noreply.github.com>
2022-11-28 11:10:01 +0100

    Update run.php
    Add WebHook Hook for module

--- run.php
+++ run.php
@@ -1,5 +1,4 @@
 <?php
-
 // Icinga Reporting | (c) 2018 Icinga GmbH | GPLv2
 
 namespace Icinga\Module\Reporting {
@@ -11,6 +10,8 @@ namespace Icinga\Module\Reporting {
     $this->provideHook('reporting/Report', '\\Icinga\\Module\\Reporting\\Reports\\SystemReport');
 
     $this->provideHook('reporting/Action', '\\Icinga\\Module\\Reporting\\Actions\\SendMail');
+
+    $this->provideHook('reporting/Action', '\\Icinga\\Module\\Reporting\\Actions\\WebHook');
 
     Icinga::app()->getLoader()->registerNamespace('reportingipl\Html', __DIR__ . '/library/vendor/ipl/Html/src');
 }
